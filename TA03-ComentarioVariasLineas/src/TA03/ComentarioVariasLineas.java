4541a95a2de161400200ba0d92749dd6eb55b40b
usuario <usuario@LAPTOP-IR96S2FN>
2022-09-07 17:47:21 +0200

    Repositorio para el proyecto "TA03 - Comentario en varias líneas".

---
+++ TA03-ComentarioVariasLineas/src/TA03/ComentarioVariasLineas.java
@@ -0,0 +1,13 @@
+package TA03;
+
+public class ComentarioVariasLineas{
+	public static void main(String[] args){
+		/* System.out.println("Hola");
+		System.out.println("Adiós");
+		System.out.println("Cucu"); */
+		for(int i=0; i<6; /*ahora viene el siguiente paso*/ i++) {
+			System.out.print(i + " ");
+		}
+		System.out.println("FIN DE PROGRAMA");
+	}
+}
